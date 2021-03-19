5a8966a47c8a30414957143daee65113e21f2eb8
MeghaGajare <gajarecmegha19@gmail.com>
2021-03-19 22:27:52 +0530

    kadane's algo added

---
+++ Array/Kadane's algorithm.py
@@ -0,0 +1,14 @@
+# kadane's algorithm
+# time complexity: O(N)
+
+ar = list(map(int,input("Enter array elements ").strip().split()))
+
+sum = 0
+maxi = ar[0]    #init with 1st element
+for i in range(len(ar)):
+    sum += ar[i]
+    if(sum > maxi):
+        maxi = sum
+    if(sum < 0):
+        sum = 0
+print('Largest sum contiguous subarray: ',maxi)
