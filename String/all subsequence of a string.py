5dc30e0fda8cec3b5e6049eae55ba2c80ba5aa71
MeghaGajare <gajarecmegha19@gmail.com>
2021-03-27 22:18:55 +0530

    all subseq.

--- String/all subsequence of a string.py
+++ String/all subsequence of a string.py
@@ -14,8 +14,8 @@ def subsequence(string,op):
     if(len(string)==0):
         print(op)
         return
-    subsequence(string[1:],op)
-    subsequence(string[1:],op+string[0])
+    subsequence(string[1:],op)                  #you can take : string[:-1],op
+    subsequence(string[1:],op+string[0])        #you can take : string[:-1], op+string[-1]
     
 
 a = input('Enter a string: ')
